96a7e4102c478977c8f65dc608c5c01a3395020b
System Administrator <admin@example.com>
2018-11-19 13:03:12 +1100

    TestParent - Build #4 Automatically created by the system

---
+++ sn/Global/Scripted REST Resource/sn-cicd/{--none--}/[CD] Pull Update Set from Source Env.-POST.js
@@ -0,0 +1,12 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_ws_operation
+ * Created On  : 2018-11-13 00:39:13
+ * Created By  : admin
+ * Updated On  : 2018-11-13 00:39:13
+ * Updated By  : admin
+ * URL         : - /sys_ws_operation.do?sys_id=aa4863e0db3d2300f526d426ca96195f
+ */
+(function process(request, response) {
+                    return new VirtualAppsDeploy(request, response).pullUpdateSet();
+                }) (request, response);
